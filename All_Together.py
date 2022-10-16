6a0ad2b3c7f947d8d03f057dd7d8e85a0fa1ee3a
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2022-10-16 19:40:42 +0530

    final sunday

--- All_Together.py
+++ All_Together.py
@@ -21,10 +21,12 @@ def timestamptodate(timestamp):
     return datetime.fromtimestamp(timestamp)
 
 
-while 1:    
+while 1:   
+    
     worksheet_1_min = sh.worksheet('1_min')
-    end = datetotimestamp(datetime.now())
-    if worksheet_1_min.acell('Q2').value == 'SHARE':
+    if worksheet_1_min.acell('Q2').value == 'SHARE': 
+        #1 min
+        end = datetotimestamp(datetime.now())
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
@@ -42,34 +44,14 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    else:
+
+
+        # 5 min
+        end = datetotimestamp(datetime.now())    
+        worksheet_1_min = sh.worksheet('5_min')
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
-        time1= worksheet_1_min.acell('P2').value
-        number= worksheet_1_min.acell('R2').value
-        hour = datetime.now().strftime('%H')
-        minute = datetime.now().strftime('%M')
-        second = datetime.now().strftime('%S')
-        
-        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
-        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
-            str(start_1_min)+'&to='+str(end)
-
-        resp_1_min = requests.get(url_1_min).json()
-        data_1_min = pd.DataFrame(resp_1_min)
-
-        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    end = datetotimestamp(datetime.now())
-
-    # 5 min
-    worksheet_1_min = sh.worksheet('5_min')
-    if worksheet_1_min.acell('Q2').value == 'SHARE':
-        year= worksheet_1_min.acell('M2').value
-        month= worksheet_1_min.acell('N2').value
-        day= worksheet_1_min.acell('O2').value
-        company= worksheet_1_min.acell('L2').value
-        time1= worksheet_1_min.acell('P2').value
         hour = datetime.now().strftime('%H')
         minute = datetime.now().strftime('%M')
         second = datetime.now().strftime('%S')
@@ -82,35 +64,14 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    else:
-        year= worksheet_1_min.acell('M2').value
-        month= worksheet_1_min.acell('N2').value
-        day= worksheet_1_min.acell('O2').value
-        time1= worksheet_1_min.acell('P2').value
-        number= worksheet_1_min.acell('R2').value
-        hour = datetime.now().strftime('%H')
-        minute = datetime.now().strftime('%M')
-        second = datetime.now().strftime('%S')
+
+        #15 min
         
-        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
-        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
-            str(start_1_min)+'&to='+str(end)
-
-        resp_1_min = requests.get(url_1_min).json()
-        data_1_min = pd.DataFrame(resp_1_min)
-
-        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-
-    #15min
-
-    worksheet_1_min = sh.worksheet('15_min')
-    end = datetotimestamp(datetime.now())
-    if worksheet_1_min.acell('Q2').value == 'SHARE':
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('15_min')
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
-        company= worksheet_1_min.acell('L2').value
-        time1= worksheet_1_min.acell('P2').value
         hour = datetime.now().strftime('%H')
         minute = datetime.now().strftime('%M')
         second = datetime.now().strftime('%S')
@@ -123,35 +84,14 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    else:
+
+        #30 min
+
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('30_min')
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
-        time1= worksheet_1_min.acell('P2').value
-        number= worksheet_1_min.acell('R2').value
-        hour = datetime.now().strftime('%H')
-        minute = datetime.now().strftime('%M')
-        second = datetime.now().strftime('%S')
-        
-        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
-        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
-            str(start_1_min)+'&to='+str(end)
-
-        resp_1_min = requests.get(url_1_min).json()
-        data_1_min = pd.DataFrame(resp_1_min)
-
-        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    
-    #30 min
-
-    worksheet_1_min = sh.worksheet('30_min')
-    end = datetotimestamp(datetime.now())
-    if worksheet_1_min.acell('Q2').value == 'SHARE':
-        year= worksheet_1_min.acell('M2').value
-        month= worksheet_1_min.acell('N2').value
-        day= worksheet_1_min.acell('O2').value
-        company= worksheet_1_min.acell('L2').value
-        time1= worksheet_1_min.acell('P2').value
         hour = datetime.now().strftime('%H')
         minute = datetime.now().strftime('%M')
         second = datetime.now().strftime('%S')
@@ -164,35 +104,13 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    else:
+        #60 min
+
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('60_min')
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
-        time1= worksheet_1_min.acell('P2').value
-        number= worksheet_1_min.acell('R2').value
-        hour = datetime.now().strftime('%H')
-        minute = datetime.now().strftime('%M')
-        second = datetime.now().strftime('%S')
-        
-        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
-        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
-            str(start_1_min)+'&to='+str(end)
-
-        resp_1_min = requests.get(url_1_min).json()
-        data_1_min = pd.DataFrame(resp_1_min)
-
-        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    
-    # 60 min
-
-    worksheet_1_min = sh.worksheet('60_min')
-    end = datetotimestamp(datetime.now())
-    if worksheet_1_min.acell('Q2').value == 'SHARE':
-        year= worksheet_1_min.acell('M2').value
-        month= worksheet_1_min.acell('N2').value
-        day= worksheet_1_min.acell('O2').value
-        company= worksheet_1_min.acell('L2').value
-        time1= worksheet_1_min.acell('P2').value
         hour = datetime.now().strftime('%H')
         minute = datetime.now().strftime('%M')
         second = datetime.now().strftime('%S')
@@ -205,35 +123,12 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    else:
+        # day
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('DAY')
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
-        time1= worksheet_1_min.acell('P2').value
-        number= worksheet_1_min.acell('R2').value
-        hour = datetime.now().strftime('%H')
-        minute = datetime.now().strftime('%M')
-        second = datetime.now().strftime('%S')
-        
-        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
-        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
-            str(start_1_min)+'&to='+str(end)
-
-        resp_1_min = requests.get(url_1_min).json()
-        data_1_min = pd.DataFrame(resp_1_min)
-
-        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
-    
-    # 1 day 
-
-    worksheet_1_min = sh.worksheet('DAY')
-    end = datetotimestamp(datetime.now())
-    if worksheet_1_min.acell('Q2').value == 'SHARE':
-        year= worksheet_1_min.acell('M2').value
-        month= worksheet_1_min.acell('N2').value
-        day= worksheet_1_min.acell('O2').value
-        company= worksheet_1_min.acell('L2').value
-        time1= worksheet_1_min.acell('P2').value
         hour = datetime.now().strftime('%H')
         minute = datetime.now().strftime('%M')
         second = datetime.now().strftime('%S')
@@ -246,12 +141,110 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+
     else:
+        worksheet_1_min = sh.worksheet('1_min')
         year= worksheet_1_min.acell('M2').value
         month= worksheet_1_min.acell('N2').value
         day= worksheet_1_min.acell('O2').value
         time1= worksheet_1_min.acell('P2').value
         number= worksheet_1_min.acell('R2').value
+        hour = datetime.now().strftime('%H')
+        minute = datetime.now().strftime('%M')
+        second = datetime.now().strftime('%S')
+        
+        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
+        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
+            str(start_1_min)+'&to='+str(end)
+
+        resp_1_min = requests.get(url_1_min).json()
+        data_1_min = pd.DataFrame(resp_1_min)
+
+        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+
+
+        #5min 
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('5_min')
+        year= worksheet_1_min.acell('M2').value
+        month= worksheet_1_min.acell('N2').value
+        day= worksheet_1_min.acell('O2').value
+        hour = datetime.now().strftime('%H')
+        minute = datetime.now().strftime('%M')
+        second = datetime.now().strftime('%S')
+        
+        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
+        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
+            str(start_1_min)+'&to='+str(end)
+
+        resp_1_min = requests.get(url_1_min).json()
+        data_1_min = pd.DataFrame(resp_1_min)
+
+        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+        #15 min
+
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('15_min')
+        year= worksheet_1_min.acell('M2').value
+        month= worksheet_1_min.acell('N2').value
+        day= worksheet_1_min.acell('O2').value
+        hour = datetime.now().strftime('%H')
+        minute = datetime.now().strftime('%M')
+        second = datetime.now().strftime('%S')
+        
+        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
+        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
+            str(start_1_min)+'&to='+str(end)
+
+        resp_1_min = requests.get(url_1_min).json()
+        data_1_min = pd.DataFrame(resp_1_min)
+
+        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+        #30 min
+
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('30_min')
+        year= worksheet_1_min.acell('M2').value
+        month= worksheet_1_min.acell('N2').value
+        day= worksheet_1_min.acell('O2').value
+        hour = datetime.now().strftime('%H')
+        minute = datetime.now().strftime('%M')
+        second = datetime.now().strftime('%S')
+        
+        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
+        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
+            str(start_1_min)+'&to='+str(end)
+
+        resp_1_min = requests.get(url_1_min).json()
+        data_1_min = pd.DataFrame(resp_1_min)
+
+        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+        #60 min
+
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('60_min')
+        year= worksheet_1_min.acell('M2').value
+        month= worksheet_1_min.acell('N2').value
+        day= worksheet_1_min.acell('O2').value
+        hour = datetime.now().strftime('%H')
+        minute = datetime.now().strftime('%M')
+        second = datetime.now().strftime('%S')
+        
+        start_1_min = datetotimestamp(datetime(int(year), int(month),int(day),int(hour),int(minute),int(second)))
+        url_1_min = 'https://priceapi.moneycontrol.com/techCharts/history?symbol='+number+'&resolution='+time1+'&from=' + \
+            str(start_1_min)+'&to='+str(end)
+
+        resp_1_min = requests.get(url_1_min).json()
+        data_1_min = pd.DataFrame(resp_1_min)
+
+        worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+        #day
+
+        end = datetotimestamp(datetime.now()) 
+        worksheet_1_min = sh.worksheet('DAY')
+        year= worksheet_1_min.acell('M2').value
+        month= worksheet_1_min.acell('N2').value
+        day= worksheet_1_min.acell('O2').value
         hour = datetime.now().strftime('%H')
         minute = datetime.now().strftime('%M')
         second = datetime.now().strftime('%S')
@@ -263,4 +256,6 @@ while 1:
         data_1_min = pd.DataFrame(resp_1_min)
 
         worksheet_1_min.update([data_1_min.columns.values.tolist()]+data_1_min.values.tolist())
+
+
     time.sleep(10)
